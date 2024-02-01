76ea2b1e97dc34e18366a448b33f853dcc66e600
SanketMhaske1 <sanketmhaske005@gmail.com>
2024-02-01 11:04:08 +0530

    commit

--- BackEnd/TaskManagementSystem/src/main/java/com/example/TaskManagementSystem/Repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.TaskManagementSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.TaskManagementSystem.model.TaskTable;
-
-public interface TaskRepository extends JpaRepository<TaskTable,Integer>{
-
-}
